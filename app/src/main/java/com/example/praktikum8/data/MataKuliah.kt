d42f1f897bea366398571e8768de26095e3a5270
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-22 01:05:30 +0700

    untuk menambah data matakuliah

---
+++ app/src/main/java/com/example/praktikum8/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum8.data
+
+object MataKuliah {
+    val Options = listOf(
+        "Pemprograman Aplikasi Mobile Lnjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
+}
