c290f40406bc786916527976555eda74904d025a
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-22 03:09:51 +0700

    Digunakan untuk menampilkan menu dropdown dengan daftar opsi yang dapat dipilih

--- app/src/main/java/com/example/praktikum8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum8/ui/view/screen/RencanaStudyView.kt
@@ -17,6 +17,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
@@ -217,5 +218,16 @@ fun DynamicSelectTextField(
                 .menuAnchor()
                 .fillMaxWidth()
         )
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = {expanded = false}) {
+            options.forEach{option: String ->
+                DropdownMenuItem(
+                    text = { Text(text = option) },
+                    onClick = {
+                        expanded = false
+                        onValueChangeEvent(option)
+                    }
+                )
+            }
+        }
     }
 }
