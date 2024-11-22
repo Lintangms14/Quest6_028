db60774477ecdd92bbc29982e2e8b26a630e6baf
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-22 15:10:41 +0700

    untuk menyimpan data Rencana Studi view

---
+++ app/src/main/java/com/example/praktikum8/ui/view/viewmodel/RencanaStudiViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.praktikum8.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.praktikum8.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudiViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMk -> stateMk.copy(Matkul = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String){
+        _krsState.update { stateKelas -> stateKelas.copy(Kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            Matkul = ls[0],
+            Kelas = ls[1]
+        )
+        }
+    }
+}
