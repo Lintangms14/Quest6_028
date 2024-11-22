9fe0f723d01ed1e2ef138e66bdce8505555f4d28
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-22 17:06:57 +0700

    Menambahkan foto profil

--- app/src/main/java/com/example/praktikum8/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/example/praktikum8/ui/view/screen/DetailView.kt
@@ -1,7 +1,28 @@
 package com.example.praktikum8.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.praktikum8.R
 import com.example.praktikum8.model.Mahasiswa
 import com.example.praktikum8.model.RencanaStudi
 
@@ -22,4 +43,24 @@ fun DetailView(
         Pair("Mata Kuliah", dataKRS.Matkul),
         Pair("Kelas", dataKRS.Kelas)
     )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profil),
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(shape = CircleShape)
+            )
+        }
+    }
 }
