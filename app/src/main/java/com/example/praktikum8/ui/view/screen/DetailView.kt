41bb27539148936be7c503621fbb199918f6fb14
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-22 17:00:10 +0700

    membuat list data Mahasiswa

---
+++ app/src/main/java/com/example/praktikum8/ui/view/screen/DetailView.kt
@@ -0,0 +1,21 @@
+package com.example.praktikum8.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.praktikum8.model.Mahasiswa
+import com.example.praktikum8.model.RencanaStudi
+
+@Composable
+fun DetailView(
+    dataMhs: Mahasiswa,
+    dataKRS: RencanaStudi,
+    mahasiswa: Mahasiswa,
+    modifier: Modifier = Modifier,
+    onBackButton: () -> Unit
+){
+    val ListDataMhs = listOf(
+        Pair("NIM", dataMhs.nim),
+        Pair("Nama", dataMhs.nama),
+        Pair("Email", dataMhs.email)
+    )
+}
