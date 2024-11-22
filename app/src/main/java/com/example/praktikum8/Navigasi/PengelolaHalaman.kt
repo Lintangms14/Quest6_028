56b1af9183505eda1b8debd4b19ae396d4141f6c
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-22 17:43:35 +0700

    membuat navHost

---
+++ app/src/main/java/com/example/praktikum8/Navigasi/PengelolaHalaman.kt
@@ -0,0 +1,52 @@
+package com.example.praktikum8.Navigasi
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum8.model.Mahasiswa
+import com.example.praktikum8.model.RencanaStudi
+import com.example.praktikum8.ui.view.screen.DetailRow
+import com.example.praktikum8.ui.view.screen.DetailView
+import com.example.praktikum8.ui.view.screen.MahasiswaFormView
+import com.example.praktikum8.ui.view.screen.RencanaStudyView
+import com.example.praktikum8.ui.view.screen.SplashView
+import com.example.praktikum8.ui.view.viewmodel.MahasiswaViewModel
+import com.example.praktikum8.ui.view.viewmodel.RencanaStudiViewModel
+
+enum class Halaman{
+    Splash,
+    Mahasiswa,
+    MataKuliah,
+    Tampil
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudiViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaStateUi.collectAsState().value
+    val krsUiState = krsViewModel.krsStateUi.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ){
+        composable(route = Halaman.Splash.name) {
+            SplashView(onMulaiButton = {
+               navController.navigate(
+                   Halaman.Mahasiswa.name
+               )
+            })
+        }
+    }
+}
